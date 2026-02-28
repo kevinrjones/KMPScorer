1baf2f4681660c2fa667aa7840cf739a70178c62
Kevin Jones <kevin@knowledgespike.com>
2026-02-28 14:05:15 +0000

    fixed use of composeApp - should be 'shsred' module

--- shared/src/commonMain/kotlin/cricket/knowledgespike/scorer/App.kt
+++ shared/src/commonMain/kotlin/cricket/knowledgespike/scorer/App.kt
@@ -15,9 +15,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.jetbrains.compose.resources.painterResource
-
-import kmpscorer.composeapp.generated.resources.Res
-import kmpscorer.composeapp.generated.resources.compose_multiplatform
+import kmpscorer.shared.generated.resources.Res
+import kmpscorer.shared.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
